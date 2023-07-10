ae1ed4252df5d367726abd439c79aec5da8eb082
SARDONYX <68905624+SARDONYX-sard@users.noreply.github.com>
2023-07-11 08:57:25 +0900

    fix(ci): fix double link gen

--- readme.md
+++ readme.md
@@ -1,7 +1,3 @@
-Fetch url: https://api.github.com/repos/SARDONYX-sard/github-userscripts/git/trees/main?recursive=1,
-  Res headers link: null
-Fetch url: https://api.github.com/repos/SARDONYX-sard/github-userscripts/git/trees/main?recursive=1,
-  Res headers link: null
 # SARDONYX userscirpts
 
 ## My original scripts
@@ -9,14 +5,14 @@ Fetch url: https://api.github.com/repos/SARDONYX-sard/github-userscripts/git/tre
 Basically works, no liability guarantee, and copyright disclaimer license (follow the userscript section of each file).
 
 <!-- original scripts -->
-- [github-oldest-button](https://github.com/SARDONYX-sard/github-userscripts/raw/main/src/github-oldest-button.user.js) 
-- [niconico-autoplay](https://github.com/SARDONYX-sard/github-userscripts/raw/main/src/niconico-autoplay.user.js) 
-- [prevent-translate-code](https://github.com/SARDONYX-sard/github-userscripts/raw/main/src/prevent-translate-code.user.js) 
-- [youtube-block-adblock-blocker](https://github.com/SARDONYX-sard/github-userscripts/raw/main/src/youtube-block-adblock-blocker.user.js) 
-- [github-oldest-button](https://github.com/SARDONYX-sard/github-userscripts/raw/main/src/github-oldest-button.user.js) 
-- [niconico-autoplay](https://github.com/SARDONYX-sard/github-userscripts/raw/main/src/niconico-autoplay.user.js) 
-- [prevent-translate-code](https://github.com/SARDONYX-sard/github-userscripts/raw/main/src/prevent-translate-code.user.js) 
-- [youtube-block-adblock-blocker](https://github.com/SARDONYX-sard/github-userscripts/raw/main/src/youtube-block-adblock-blocker.user.js) 
+- [github-oldest-button](https://github.com/SARDONYX-sard/github-userscripts/raw/main/src/github-oldest-button.user.js)
+- [niconico-autoplay](https://github.com/SARDONYX-sard/github-userscripts/raw/main/src/niconico-autoplay.user.js)
+- [prevent-translate-code](https://github.com/SARDONYX-sard/github-userscripts/raw/main/src/prevent-translate-code.user.js)
+- [youtube-block-adblock-blocker](https://github.com/SARDONYX-sard/github-userscripts/raw/main/src/youtube-block-adblock-blocker.user.js)
+- [github-oldest-button](https://github.com/SARDONYX-sard/github-userscripts/raw/main/src/github-oldest-button.user.js)
+- [niconico-autoplay](https://github.com/SARDONYX-sard/github-userscripts/raw/main/src/niconico-autoplay.user.js)
+- [prevent-translate-code](https://github.com/SARDONYX-sard/github-userscripts/raw/main/src/prevent-translate-code.user.js)
+- [youtube-block-adblock-blocker](https://github.com/SARDONYX-sard/github-userscripts/raw/main/src/youtube-block-adblock-blocker.user.js)
 <!-- original scripts end -->
 
 ## Forked scripts
@@ -24,8 +20,6 @@ Basically works, no liability guarantee, and copyright disclaimer license (follo
 Follow the original license.
 
 <!-- forked scripts -->
-- [github-in-vscode](https://github.com/SARDONYX-sard/github-userscripts/raw/main/src/fork/github-in-vscode.user.js) 
-- [github-in-vscode](https://github.com/SARDONYX-sard/github-userscripts/raw/main/src/fork/github-in-vscode.user.js) 
+- [github-in-vscode](https://github.com/SARDONYX-sard/github-userscripts/raw/main/src/fork/github-in-vscode.user.js)
+- [github-in-vscode](https://github.com/SARDONYX-sard/github-userscripts/raw/main/src/fork/github-in-vscode.user.js)
 <!-- forked scripts end -->
-
-
--- scripts/genelate-scripts-link.js
+++ scripts/genelate-scripts-link.js
@@ -1,123 +1,106 @@
 // @ts-check
 
-/** @type {"debug"|"info"|"warn"|"error"} */
-const DEBUG_MODE = "debug";
-const INTERVAL_TIME = 1000;
-const CLEAR_INTERVAL_TIME = 1000;
-
-const log = {
-  debug: (...msg) => {
-    /(?:debug)/.test(DEBUG_MODE) && console.debug(...msg);
-  },
-  info: (...msg) => {
-    /(?:debug|info)/.test(DEBUG_MODE) && console.info(...msg);
-  },
-  warn: (...msg) => {
-    /(?:debug|info|warn)/.test(DEBUG_MODE) && console.warn(...msg);
-  },
-  error: (...msg) => {
-    /(?:debug|info|warn|error)/.test(DEBUG_MODE) && console.error(...msg);
-  },
-};
-
-/** See: https://developer.mozilla.org/en-US/docs/Glossary/Base64 */
-function base64ToBytes(/** @type {string} */ base64) {
-  const binString = atob(base64);
-  // @ts-ignore
-  return Uint8Array.from(binString, (m) => m.codePointAt(0));
-}
-
-function bytesToBase64(bytes) {
-  const binString = Array.from(bytes, (x) => String.fromCodePoint(x)).join("");
-  return btoa(binString);
-}
-
-/// ref: https://stackoverflow.com/questions/25022016/get-all-file-names-from-a-github-repo-through-the-github-api
-async function createMarkdownFileLinks() {
-  const url =
-    "https://api.github.com/repos/SARDONYX-sard/github-userscripts/git/trees/main?recursive=1";
-
-  const res = await fetch(url, {
-    headers: {
-      Accept: "application/vnd.github.v3+json",
-    },
-  });
-
-  log.info(`Fetch url: ${url},
-  Res headers link: ${res.headers.get("Link")}`);
-
-  /**
-   * ref: https://github.com/microsoft/TypeScript/issues/50321
-   * @typedef {{
-   *   sha: string;
-   *   url: string;
-   *   tree: ({
-   *       path: string;
-   *       mode: string;
-   *       type: string;
-   *       sha: string;
-   *       size?: number;
-   *       url: string;
-   *   })[];
-   *   truncated: boolean;
-   * }} FileTree
-   */
-  /** @type {FileTree} */
-  const fileTree = await res.json();
-  const rawLink = "https://github.com/SARDONYX-sard/github-userscripts/raw/main";
-
-  let myLinksMd = "";
-  let forkLinksMd = "";
-  fileTree.tree.forEach((fileInfo) => {
-    if (fileInfo.path.endsWith("user.js")) {
-      const [_, _dir, fileName, _ext] = fileInfo.path.match(/(.*\/)(.*)(\.user\.js)/) ?? [];
-      if (!fileName) {
-        log.error("Failed to get file name");
-        return;
-      }
-
-      const linkMd = `- [${fileName}](${rawLink}/${fileInfo.path}) \n`;
-
-      if (fileInfo.path.startsWith("src/fork")) {
-        forkLinksMd += linkMd;
-      } else {
-        myLinksMd += linkMd;
-      }
-    }
-  });
-
-  /** @typedef {{
-   *     name: string;
-   *     path: string;
-   *     sha: string;
-   *     size?: number;
-   *     url: string;
-   *     html_url: string;
-   *     git_url: string;
-   *     download_url: string;
-   *     type: string;
-   *     content: string;
-   *     encoding: string;
-   *     _links: {
-   *         self: string;
-   *         git: string;
-   *         html: string;
-   *     };
-   * }} GitHubContent
-   */
-  /** @type {GitHubContent} */
-  const readmeRes = await (
-    await fetch("https://api.github.com/repos/SARDONYX-sard/github-userscripts/contents/readme.md")
-  ).json();
-  const readmeMd = new TextDecoder().decode(base64ToBytes(readmeRes.content));
-  const forkLinksComment = "<!-- forked scripts -->\n";
-  const myLinksComment = "<!-- original scripts -->\n";
-
-  return readmeMd
-    .replace(myLinksComment, myLinksComment + myLinksMd)
-    .replace(forkLinksComment, forkLinksComment + forkLinksMd);
-}
-
 (async () => {
+  /** See: https://developer.mozilla.org/en-US/docs/Glossary/Base64 */
+  function base64ToBytes(/** @type {string} */ base64) {
+    const binString = atob(base64);
+    // @ts-ignore
+    return Uint8Array.from(binString, (m) => m.codePointAt(0));
+  }
+
+  function bytesToBase64(bytes) {
+    const binString = Array.from(bytes, (x) => String.fromCodePoint(x)).join("");
+    return btoa(binString);
+  }
+
+  /// ref: https://stackoverflow.com/questions/25022016/get-all-file-names-from-a-github-repo-through-the-github-api
+  async function createMarkdownFileLinks() {
+    const url =
+      "https://api.github.com/repos/SARDONYX-sard/github-userscripts/git/trees/main?recursive=1";
+
+    const res = await fetch(url, {
+      headers: {
+        Accept: "application/vnd.github.v3+json",
+      },
+    });
+
+    /**
+     * ref: https://github.com/microsoft/TypeScript/issues/50321
+     * @typedef {{
+     *   sha: string;
+     *   url: string;
+     *   tree: ({
+     *       path: string;
+     *       mode: string;
+     *       type: string;
+     *       sha: string;
+     *       size?: number;
+     *       url: string;
+     *   })[];
+     *   truncated: boolean;
+     * }} FileTree
+     */
+    /** @type {FileTree} */
+    const fileTree = await res.json();
+    const rawLink = "https://github.com/SARDONYX-sard/github-userscripts/raw/main";
+
+    let myLinksMd = "";
+    let forkLinksMd = "";
+    fileTree.tree.forEach((fileInfo) => {
+      if (fileInfo.path.endsWith("user.js")) {
+        const [_, _dir, fileName, _ext] = fileInfo.path.match(/(.*\/)(.*)(\.user\.js)/) ?? [];
+        if (!fileName) {
+          throw new Error("Failed to get file name");
+        }
+
+        const linkMd = `- [${fileName}](${rawLink}/${fileInfo.path})\n`;
+
+        if (fileInfo.path.startsWith("src/fork")) {
+          forkLinksMd += linkMd;
+        } else {
+          myLinksMd += linkMd;
+        }
+      }
+    });
+
+    /** @typedef {{
+     *     name: string;
+     *     path: string;
+     *     sha: string;
+     *     size?: number;
+     *     url: string;
+     *     html_url: string;
+     *     git_url: string;
+     *     download_url: string;
+     *     type: string;
+     *     content: string;
+     *     encoding: string;
+     *     _links: {
+     *         self: string;
+     *         git: string;
+     *         html: string;
+     *     };
+     * }} GitHubContent
+     */
+    /** @type {GitHubContent} */
+    const readmeRes = await (
+      await fetch(
+        "https://api.github.com/repos/SARDONYX-sard/github-userscripts/contents/readme.md"
+      )
+    ).json();
+    const readmeMd = new TextDecoder().decode(base64ToBytes(readmeRes.content));
+    const forkLinkStart = "<!-- forked scripts -->\n";
+    const forkLinkEnd = "<!-- forked scripts end -->\n";
+    const forkLinksComment = new RegExp(`${forkLinkStart}*.${forkLinkEnd}`);
+    const myLinkStart = "<!-- original scripts -->\n";
+    const myLinkEnd = "<!-- original scripts end -->\n";
+    const myLinksComment = new RegExp(`${myLinkStart}*.${myLinkEnd}`);
+
+    return readmeMd
+      .replace(myLinksComment, `${myLinkStart}${myLinksMd}${myLinkEnd}`)
+      .replace(forkLinksComment, `${forkLinkStart}${forkLinksMd}${forkLinkEnd}`)
+      .trim();
+  }
+
   console.log(await createMarkdownFileLinks());
 })();
